b3b606833cf10d28f890a1c9aa0ec9753bca914d
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2024-11-11 18:58:22 -0500

    Blep

--- src/client/java/stellarwitch7/illusionist/IllusionistClient.java
+++ src/client/java/stellarwitch7/illusionist/IllusionistClient.java
@@ -3,8 +3,8 @@ package stellarwitch7.illusionist;
 import net.fabricmc.api.ClientModInitializer;
 
 public class IllusionistClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    }
 }
--- src/main/java/stellarwitch7/illusionist/Illusionist.java
+++ src/main/java/stellarwitch7/illusionist/Illusionist.java
@@ -8,17 +8,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Illusionist implements ModInitializer {
-	public static final String MOD_ID = "illusionist";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final String MOD_ID = "illusionist";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
 
-		ModTricks.register();
-	}
+        ModTricks.register();
+    }
 
     public static Identifier id(String path) {
         return Identifier.of(MOD_ID, path);
