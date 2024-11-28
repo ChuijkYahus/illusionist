69761219798bc4f0fb61a66cfb52baffbde351f6
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2024-11-27 20:39:58 -0500

    Bump Trickster dependency and notice issues ;-;

--- src/main/java/stellarwitch7/illusionist/Illusionist.java
+++ src/main/java/stellarwitch7/illusionist/Illusionist.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import stellarwitch7.illusionist.spell.trick.ModTricks;
 
-
+// testing spell: `YwqT9+bf4MHEysjIz8DAwMwIJBgYGRlBwgwuDlzIYlrPvmiCxD9wNSxnYmTDVH6AAV2MIwTEYWBihJKMzOgqAngcOpFVYLMwwMOhk5GRE10rowsTKzmOwGUd2EwGByL1AwC+fb+wOgEAAA==`
 public class Illusionist implements ModInitializer {
     public static final String MOD_ID = "illusionist";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
